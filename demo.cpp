4244b05abe33c931f7f2c8fe7097743281a10bf9
WeirdPenguins <vinhd220@gmail.com>
2024-04-29 22:44:21 +0700

    Code ham gameover

--- demo.cpp
+++ demo.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <conio.h>
 using namespace std;
-void gotoxy( int column, int line ); //H√†m goto
+void gotoxy( int column, int line ); //H‡m goto
 
 
 
@@ -16,7 +16,7 @@ private:
 
     struct Ran {
         Diem body[100];
-        int length; //ƒê·ªô d√†i r·∫Øn
+        int length; //–? d‡i r?n
     };
 
     Ran ran;
@@ -26,7 +26,7 @@ private:
     Diem Food;
 
 public:
-    CONRAN() { // H√†m kh·ªüi t·∫°o gi√° tr·ªã ban ƒë·∫ßu
+    CONRAN() { // H‡m kh?i t?o gi· tr? ban d?u
         ran.length = 4;
         toc_do = 300;
       //  tang_toc = 4;
@@ -101,6 +101,101 @@ void CONRAN::startGame() {
 
 
 
+=======
+
+void CONRAN::VeKhung() { //H‡m v? tu?ng gi?i h?n khu v?c choi
+    for (int i = 10; i < 105; i++) {
+        gotoxy(i, 1);
+        cout << "+";
+        gotoxy(i, 26);
+        cout << "+";
+    }
+    for (int i = 1; i < 26; i++) {
+        gotoxy(10, i);
+        cout << "+";
+        gotoxy(104, i);
+        cout << "+";
+    }
+}
+
+
+
+void CONRAN::TaoRan() { // H‡m t?o r?n
+    int x_head = 50;
+    int y_head = 10;
+    for (int i = 0; i < ran.length; i++) {
+        ran.body[i].x = x_head--;
+        ran.body[i].y = y_head;
+      }
+}
+
+void CONRAN::VeRan() { // H‡m v? r?n
+    for (int i = 0; i < ran.length; i++) {
+        gotoxy(ran.body[i].x, ran.body[i].y);
+        if (i == 0) {
+            cout << "0";
+        }
+        else if ( i == ran.length - 1 ) {
+            cout << -;
+        }
+         else {
+            cout << "O";
+        }
+    }
+}
+
+void CONRAN::DiChuyen(int x, int y) { // H‡m di chuy?n c?a r?n
+    for (int i = ran.length - 1; i > 0; i--) {
+        ran.body[i] = ran.body[i - 1];
+    }
+    ran.body[0].x = x;
+    ran.body[0].y = y;
+}
+
+bool CONRAN::gameover() { // R?n ch?t v‡ chuong trÏnh k?t th˙c khi r?n t? c?n ph?i mÏnh ho?c ch?m tu?ng
+      for (int i = 1; i < ran.length; i++) {
+        if (ran.body[i].x == ran.body[0].x && ran.body[i].y == ran.body[0].y) {
+            return true;
+        }
+    }
+    if (ran.body[0].x == 10 || ran.body[0].x == 105 || ran.body[0].y == 1 || ran.body[0].y == 26) {
+        return true;
+    }
+    return false;
+}
+
+
+void CONRAN::VeMoi() {
+    gotoxy(Food.x, Food.y);
+    cout << "X";
+
+}
+
+void CONRAN::AnMoi() {   // R?n an m?i thÏ s? d‡i ra thÍm 1 d?t
+    if (ran.body[0].x == Food.x && ran.body[0].y == Food.y) {
+        ran.length++;
+        do {
+            Food.x = rand() % (104 - 11 + 1) + 11;
+            Food.y = rand() % (25 - 2 + 1) + 2;
+        } while (KiemTraMoi());
+        VeMoi();
+    }
+}
+
+
+
+bool CONRAN::KiemTraMoi() {  // Ki?m tra xem m?i cÛ b? tr˘ng v?i th‚n r?n khÙng
+
+   for (int i = 0; i < ran.length; ++i) {
+        if (Food.x == ran.body[i].x && Food.y == ran.body[i].y) {
+            return true;
+        }
+    }
+    return false;
+
+}
+
+
 void CONRAN::Score() {
     if (ran.body[0].x == Food.x && ran.body[0].y == Food.y) {
         gotoxy(107, 2);
@@ -110,8 +205,46 @@ void CONRAN::Score() {
 }
 
 int main() {
+    char choice;
+    CONRAN game;
+
+    do {
+        system("cls");
+        cout << "\t\t\t\t\t\t\tMenu\n";
+        cout << "\t\t\t\t\t\t1. Start Game\n";
+        cout << "\t\t\t\t\t\t2. Exit\n";
+        cout << "\t\t\t\t\t\t3. Infomation\n";
+        cout << "\t\t\t\t\t\tEnter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+        case '1':
+            game.startGame();
+            break;
+        case '2':
+            cout << "Exiting...\n";
+            return 0;
+        case '3':
+            cout << "Day la bai lam SnakeGame don gian c?a nhom.\n";
+            cout << "Gom cac thanh vien : \n";
+            cout << "1.Duong Phat Vinh\n";
+            cout << "2.Nguyen Dai Tung\n";
+            cout << "3.Nguyen Huynh Minh Thu\n";
+            cout << "4.Do Van Vu\n";
+            wcout << "\t\t\t\tHUONG DAN" << endl;
+            cout << "\tDung phim mui ten de di chuyen ran an moi nhieu nhat co the nhung khong de\n\tcham than vao tuong" << endl;
+            cout << "Chuc cac ban may man";
+            return 0;
+        default:
+            cout << "Invalid choice. Please enter again.\n";
+            break;
+        }
+    } while (choice != '2' || choice != '3');
+
+
     CONRAN game;
     game.startGame();
+
     _getch();
     return 0;
 }
