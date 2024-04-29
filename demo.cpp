807cec1b678860737d2000c8aac46102c68e73d6
WeirdPenguins <vinhd220@gmail.com>
2024-04-29 22:15:40 +0700

    Fix ham kiem tra moi

--- demo.cpp
+++ demo.cpp
@@ -127,7 +127,7 @@ void CONRAN::AnMoi() {   // Rắn ăn mồi thì sẽ dài ra thêm 1 đốt
 
 }
 
-bool CONRAN::KiemTraMoi(int x, int y) {  // Kiểm tra xem mồi có bị trùng với thân rắn không
+bool CONRAN::KiemTraMoi() {  // Kiểm tra xem mồi có bị trùng với thân rắn không
    for (int i = 0; i < ran.length; ++i) {
         if (Food.x == ran.body[i].x && Food.y == ran.body[i].y) {
             return true;
